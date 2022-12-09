4b15e9afdf7ac116986aa2617f160ea30cadf324
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-12-09 21:59:08 +0700

    add code for basic bookstore api

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!");
+});
+
+/* Book router */
+const bookRouter = require("./book.api.js");
+router.use("/books", bookRouter);
+
+module.exports = router;
